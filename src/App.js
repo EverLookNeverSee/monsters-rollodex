144993a2e375093f0b4429be959c1d52098a7cd9
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-05-06 13:39:34 +0330

    Using map method

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import {Component} from "react";
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {name: {firstName: "Milad", lastName: "Sadeghi DM"}, company: "Meta"};
-    }
-  render() {
-    return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Hi, my name is {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company} ;)
-            </p>
-            <button onClick={() => {
-                this.setState(() => {
-                    return this.setState({name: {firstName: "EverLook", lastName: "NeverSee"}});
-                }, () => {
-                    console.log(this.state)});
-            }}>
-                Change the name
-            </button>
-          </header>
-        </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			monsters: [{name: "Linda"}, {name: "Frank"}, {name: "Jacky"}, {name: "Andrei"}],
+		};
+	}
+	render() {
+		return (
+			<div className="App">
+				{this.state.monsters.map(monster => {
+					return <h1>{monster.name}</h1>;
+				})}
+			</div>
+		);
+	}
 }
 
 export default App;
